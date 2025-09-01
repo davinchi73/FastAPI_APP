f5a7bebd0623b14d67f2cfc35521b4a55def40e8
Devinn Chi <70714199+davinchi73@users.noreply.github.com>
2025-09-01 15:35:44 -0400

    update post.py

--- example_apps/post.py
+++ example_apps/post.py
@@ -15,16 +15,6 @@ class Item(BaseModel):
 # in-memory storage, youd use a database instead in real apps
 items = []
 
-# "/" is the root url of the app
-@app.get("/")
-def read_root():
-    return {"message" : "Hello World"} # returns simple info in json format
-
-# get request for /items page, along with any item id following it in the url
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q} # returns simple info in json format, the item id and any query parameters or none if none
-
 @app.post("/items")
 def create_items(item: Item):
     # add a new item to our local storage
